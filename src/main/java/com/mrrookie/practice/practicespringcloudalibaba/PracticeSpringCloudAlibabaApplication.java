9b0fc1a256a7d59d5ce6ecd127428e632034d9b3
wangpengcheng <wangpengcheng@lixiang.com>
2020-04-14 18:13:07 +0800

    Initial commit

---
+++ src/main/java/com/mrrookie/practice/practicespringcloudalibaba/PracticeSpringCloudAlibabaApplication.java
@@ -0,0 +1,13 @@
+package com.mrrookie.practice.practicespringcloudalibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeSpringCloudAlibabaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeSpringCloudAlibabaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mrrookie/practice/practicespringcloudalibaba/PracticeSpringCloudAlibabaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrrookie.practice.practicespringcloudalibaba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeSpringCloudAlibabaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
